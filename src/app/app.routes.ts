d238e69e1d25fa6c64a0805c83c4f66432e1ba0d
Moein <64212565+moeinpourhamzeh@users.noreply.github.com>
2025-05-07 14:58:45 +0330

    remove last session's files and components and codes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,5 @@
 import { Routes } from '@angular/router';
-import { AddComponent } from './_pages/add/add.component';
-import { ListComponent } from './_pages/list/list.component';
 
 export const routes: Routes = [
-  { path: "", redirectTo: "list", pathMatch: "full" },
-  { path: "add", component: AddComponent },
-  { path: "list", component: ListComponent }
+  
 ];
