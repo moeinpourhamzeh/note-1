e9bb3f8cd7c887894a4f7e08600d5d9039a1de0c
Moein <64212565+moeinpourhamzeh@users.noreply.github.com>
2025-05-28 15:29:49 +0330

    list and add components added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from './_pages/list/list.component';
+import { AddComponent } from './_pages/add/add.component';
 
 export const routes: Routes = [
-  
+  { path: '', redirectTo: 'list', pathMatch: 'full' },
+  { path: 'list', component: ListComponent },
+  { path: 'add', component: AddComponent }
 ];
